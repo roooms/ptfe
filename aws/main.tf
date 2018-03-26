bca4eea4c2d8b42304a5922ba759a6dfe261d32b
Dan Brown <roooms@gmail.com>
2018-03-26 09:31:23 +0100

    Use modules for different install type support

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.9.3"
+  required_version = ">= 0.10.3"
 }
 
 provider "aws" {
@@ -22,209 +22,72 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/user-data.tpl")}"
 
   vars {
-    hostname       = "${var.service_name}.hashidemos.io"
+    hostname       = "${var.namespace}.hashidemos.io"
     replicated_pwd = "${random_pet.replicated-pwd.id}"
   }
 }
 
 #------------------------------------------------------------------------------
-# vpc
+# network 
 #------------------------------------------------------------------------------
 
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
-  tags {
-    Name = "${var.service_name}-vpc"
-  }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  tags {
-    Name = "${var.service_name}-internet_gateway"
-  }
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.main.id}"
-  }
-
-  tags {
-    Name = "${var.service_name}-route_table"
-  }
-}
-
-resource "aws_subnet" "subnet" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id     = "${aws_vpc.main.id}"
-
-  tags {
-    Name = "${var.service_name}-subnet"
-  }
-
-  map_public_ip_on_launch = true
-}
-
-resource "aws_route_table_association" "subnet" {
-  route_table_id = "${aws_route_table.main.id}"
-  subnet_id      = "${aws_subnet.subnet.id}"
+module "network" {
+  source    = "network/"
+  namespace = "${var.namespace}"
 }
 
 #------------------------------------------------------------------------------
-# security group
+# demo/poc ptfe 
 #------------------------------------------------------------------------------
 
-resource "aws_security_group" "main" {
-  name        = "${var.service_name}-sg"
-  description = "${var.service_name} security group"
-  vpc_id      = "${aws_vpc.main.id}"
-
-  ingress {
-    protocol  = -1
-    from_port = 0
-    to_port   = 0
-    self      = true
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 8800
-    to_port     = 8800
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "demo" {
+  source                 = "demo/"
+  namespace              = "${var.namespace}"
+  aws_instance_ami       = "${var.aws_instance_ami}"
+  aws_instance_type      = "${var.aws_instance_type}"
+  subnet_id              = "${module.network.subnet_ids[0]}"
+  vpc_security_group_ids = "${module.network.security_group_id}"
+  user_data              = ""
+  ssh_key_name           = "${var.ssh_key_name}"
+  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
+  owner                  = "${var.owner}"
+  ttl                    = "${var.ttl}"
 }
 
 #------------------------------------------------------------------------------
-# demo ptfe resources
+# production mounted disk ptfe 
 #------------------------------------------------------------------------------
 
-resource "aws_instance" "demo" {
-  ami                    = "${var.aws_instance_ami}"
-  instance_type          = "${var.aws_instance_type}"
-  subnet_id              = "${aws_subnet.subnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.main.id}"]
-  key_name               = "${var.ssh_key_name}"
-  user_data              = "${data.template_file.user_data.rendered}"
-
-  root_block_device {
-    volume_size = 80
-    volume_type = "gp2"
-  }
-
-  tags {
-    Name  = "${var.service_name}-demo-instance"
-    owner = "${var.owner}"
-    TTL   = "${var.ttl}"
-  }
-}
-
-resource "aws_eip" "demo" {
-  instance = "${aws_instance.demo.id}"
-  vpc      = true
-}
-
-resource "aws_route53_record" "demo" {
-  zone_id = "${data.aws_route53_zone.hashidemos.zone_id}"
-  name    = "${var.service_name}.hashidemos.io."
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_eip.demo.public_ip}"]
+module "pmd" {
+  source                 = "pmd/"
+  namespace              = "${var.namespace}"
+  aws_instance_ami       = "${var.aws_instance_ami}"
+  aws_instance_type      = "${var.aws_instance_type}"
+  subnet_id              = "${module.network.subnet_ids[0]}"
+  vpc_security_group_ids = "${module.network.security_group_id}"
+  user_data              = ""
+  ssh_key_name           = "${var.ssh_key_name}"
+  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
+  owner                  = "${var.owner}"
+  ttl                    = "${var.ttl}"
 }
 
 #------------------------------------------------------------------------------
-# prod-mounted-disk ptfe resources
+# production external-services ptfe 
 #------------------------------------------------------------------------------
 
-
-#resource "aws_instance" "prod-mounted-disk" {
-#  ami                    = "${var.aws_instance_ami}"
-#  instance_type          = "${var.aws_instance_type}"
-#  subnet_id              = "${aws_subnet.subnet.id}"
-#  vpc_security_group_ids = ["${aws_security_group.main.id}"]
-#  key_name               = "${var.ssh_key_name}"
-#
-#  root_block_device {
-#    volume_size = 80
-#    volume_type = "gp2"
-#  }
-#
-#  tags {
-#    Name = "${var.service_name}-prod-mounted-disk-instance"
-#    owner = "${var.owner}"
-#    TTL   = "${var.ttl}"
-#  }
-#}
-#
-#resource "aws_ebs_volume" "prod-mounted-disk" {
-#  availability_zone = "${aws_instance.prod-mounted-disk.availability_zone}"
-#  size              = 88
-#  type              = "gp2"
-#
-#  tags {
-#    Name = "${var.service_name}-ebs_volume"
-#  }
-#}
-#
-#resource "aws_volume_attachment" "prod-mounted-disk" {
-#  device_name = "/dev/xvdb"
-#  instance_id = "${aws_instance.prod-mounted-disk.id}"
-#  volume_id   = "${aws_ebs_volume.prod-mounted-disk.id}"
-#}
-
-
-#------------------------------------------------------------------------------
-# prod-external-services ptfe resources
-#------------------------------------------------------------------------------
-
-
-#resource "aws_instance" "prod-external-services" {
-#  ami                    = "ami-4262d53f"
-#  instance_type          = "t2.large"
-#  subnet_id              = "${aws_subnet.subnet-a.id}"
-#  vpc_security_group_ids = ["${aws_security_group.main.id}"]
-#  key_name               = "dbrown-hc-eu-west-3"
-#
-#  root_block_device {
-#    volume_size = 80
-#    volume_type = "gp2"
-#  }
-#
-#  tags {
-#    Name = "${var.service_name}-prod-external-services-instance"
-#  }
-#}
+module "pes" {
+  source                 = "pes/"
+  namespace              = "${var.namespace}"
+  aws_instance_ami       = "${var.aws_instance_ami}"
+  aws_instance_type      = "${var.aws_instance_type}"
+  subnet_ids             = "${module.network.subnet_ids}"
+  vpc_security_group_ids = "${module.network.security_group_id}"
+  user_data              = ""
+  ssh_key_name           = "${var.ssh_key_name}"
+  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
+  database_pwd           = "${random_pet.replicated-pwd.id}"
+  db_subnet_group_name   = "${module.network.db_subnet_group_id}"
+  owner                  = "${var.owner}"
+  ttl                    = "${var.ttl}"
+}
--- aws/user-data.tpl
+++ aws/user-data.tpl
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-# install certbot
-apt-get update
-apt-get install software-properties-common --yes
-add-apt-repository ppa:certbot/certbot --yes
-apt-get update
-apt-get install certbot --yes
-
-# generate certificate and key
-certbot certonly -n -d ${hostname} -m null@null.com --standalone --agree-tos
-
 # create replicated unattended installer config
 cat > /etc/replicated.conf <<EOF
 {
   "DaemonAuthenticationType": "password",
-  "DaemonAuthenticationPassword": "${replicated_pwd}",
-  "TlsBootstrapType": "server-path",
-  "TlsBootstrapHostname": "${hostname}",
-  "TlsBootstrapCert": "/etc/letsencrypt/live/${hostname}/fullchain.pem",
-  "TlsBootstrapKey": "/etc/letsencrypt/live/${hostname}/privkey.pem",
+  "DaemonAuthenticationPassword": "ptfe-pwd",
+  "TlsBootstrapType": "self-signed",
   "LogLevel": "debug",
+  "ImportSettingsFrom": "/tmp/replicated-settings.json",
+  "LicenseFileLocation": "/tmp/license.rli"
   "BypassPreflightChecks": true
+}
+EOF
+cat > /tmp/replicated-settings.json <<EOF
+{
+  "hostname": {
+    "value": "dan-ptfe-pes.hashidemos.io"
+  }
+  "installation_type": {
+    "value": "production"
+  },
+  "production_type": {
+    "value": "disk"
+  },
+  "disk_path": {
+    "value": "/data"
+  },
+  "letsencrypt_auto": {
+    "value": "1"
+  },
+  "letsencrypt_email": {
+    "value": "null@null.com"
+  },
 }
 EOF
 
